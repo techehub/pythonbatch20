0b301b604a251c2dfb41944275eeee43feb239ee
techehub <vijeesh.tp@techehub.com>
2019-07-16 15:43:54 +0530

    while and for example

---
+++ while.py
@@ -0,0 +1,43 @@
+"""
+This program is to calculate
+number from 1
+"""
+
+marks=[10,33,44,77,55]
+totalMark=0
+for mark in marks :
+    totalMark= totalMark      + mark
+print ("Total Mark is ", totalMark)
+
+
+'''
+
+print (num)
+print (type (num))
+print (len(num))
+
+i =0
+sum = 0
+while i < len (num) :
+    #print (num[i])
+    if (num[i]%2==0):
+        sum=sum+num[i]
+    i = i + 1
+
+print (sum)
+'''
+
+
+"""
+while (num<= 1000):
+    print (num)
+    num = num + 1
+    if (num%2==0):
+        esum= esum+num
+    else :
+        osum=osum+num
+
+print ("sum even num is", esum)
+print ("sum odd nums is", osum)
+
+"""
